a7eea76f8d3a00c0136d26639c1b587c11392bd6
StoshFerhobin <sfdatz@live.com>
2020-02-04 19:29:31 -0600

    First

---
+++ RoyalRodents/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterControllerTMP controller;
+    public float moveSpeed = 40f;
+
+    private float horizontalMove = 0f;
+    private bool jump = false;
+    private bool crouch = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove= Input.GetAxisRaw("Horizontal") *moveSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            crouch = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.S))
+        {
+            crouch = false;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        // move our character
+        controller.Move(horizontalMove *Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
